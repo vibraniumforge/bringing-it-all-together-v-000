86ee39f21dd84886fb75eb6408553b5bd50d9dea
Greg Morgan <gregm9999@gmail.com>
2018-11-22 18:04:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,12 +71,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    attributes = {
-      :id=> row[0],
-      :name => row[1],
-      :breed => row[2]
-    }
-    self.new(attributes)
+    id = row[0]
+    name= row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name(name)
