f97e62d8d2fbb59c4e974c0961089dfca9c6b37a
Greg Morgan <gregm9999@gmail.com>
2018-11-22 18:03:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,8 +65,9 @@ class Dog
       dog_data=dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      new_dog = self.create(:name=> name, :breed=>breed)
+      dog = self.create(:name=> name, :breed=>breed)
     end
+    dog
   end
 
   def self.new_from_db(row)
